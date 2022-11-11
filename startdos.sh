537fcf95b8d18eb0d55c760f3647987e068f6f5e
davidegat <51516281+davidegat@users.noreply.github.com>
2022-11-12 00:09:31 +0100

    Update startdos.sh

--- startdos.sh
+++ startdos.sh
@@ -1,3 +1,3 @@
 # This file goes into /home/root on reMarkable
 cd /home/root/remarkados # Adjust as needed
-bochs -q -unlock -f bochsrc
+bochs -q -unlock -f bochsrc # -q: enables quickboot, -unlock: prevents lock errors, -f: config file
